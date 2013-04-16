465ec2f9c62870cafc78b3519cfa37eb57f4f2a6
spacecaker <robin-barry@hotmail.com>
2013-04-16 12:53:46 +0200

    Upload first device config for kumquat csm9

---
+++ BoardConfig.mk
@@ -0,0 +1,33 @@
+USE_CAMERA_STUB := true
+
+# inherit from the proprietary version
+-include vendor/sony/kumquat/BoardConfigVendor.mk
+
+#inherit from the common montblanc definitions
+-include device/sony/montblanc-common/BoardConfigCommon.mk
+
+TARGET_SPECIFIC_HEADER_PATH += device/sony/kumquat/include
+
+TARGET_KERNEL_CONFIG := cyanogen_kumquat_defconfig
+
+# Partition information
+BOARD_VOLD_MAX_PARTITIONS := 16
+
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x01400000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x01400000
+BOARD_CMDLINE := device/sony/kumquat/config/cmdline.txt
+
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1056964608
+BOARD_USERDATA_PARTITION_SIZE := 2147483648
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_FORCE_KERNEL_ADDRESS  := 0x00008000
+BOARD_FORCE_RAMDISK_ADDRESS := 0x01000000
+
+COMMON_GLOBAL_CFLAGS += -DNEW_NOTIFICATION -DSECOND_NOTIFICATION
+
+BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_USERIMAGES_USE_EXT4 := true
+
+BOARD_SDCARD_INTERNAL_DEVICE := /dev/block/mmcblk0p14
+
+TARGET_OTA_ASSERT_DEVICE := ST25i,ST25a,kumquat
---
+++ kumquat.mk
@@ -0,0 +1,89 @@
+#
+# Copyright (C) 2011 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit the proprietary counterpart
+$(call inherit-product-if-exists, vendor/sony/kumquat/kumquat-vendor.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/sony/kumquat/overlay
+
+# Inherit the montblanc-common definitions
+$(call inherit-product, device/sony/montblanc-common/montblanc.mk)
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_eu_supl.mk)
+
+# These are the hardware-specific features
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
+
+# This device is hdpi.  However the platform doesn't
+# currently contain all of the bitmaps at hdpi density so
+# we do this little trick to fall back to the hdpi version
+# if the hdpi doesn't exist.
+PRODUCT_AAPT_CONFIG := normal mdpi hdpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
+
+# Configuration scripts
+PRODUCT_COPY_FILES += \
+   device/sony/montblanc-common/prebuilt/logo-480x854.rle:root/logo.rle
+
+# Configuration scripts
+PRODUCT_COPY_FILES += \
+   $(LOCAL_PATH)/config/dash.conf:system/etc/dash.conf \
+   $(LOCAL_PATH)/prebuilt/hw_config.sh:system/etc/hw_config.sh
+
+# USB function switching
+PRODUCT_COPY_FILES += \
+   $(LOCAL_PATH)/config/init.st-ericsson.usb.rc:root/init.st-ericsson.usb.rc
+
+PRODUCT_COPY_FILES += \
+   $(LOCAL_PATH)/config/vold.fstab:system/etc/vold.fstab \
+   $(LOCAL_PATH)/config/media_profiles.xml:system/etc/media_profiles.xml
+
+# Recovery bootstrap (device-specific part)
+PRODUCT_COPY_FILES += \
+   $(LOCAL_PATH)/recovery/bootrec-device:root/sbin/bootrec-device \
+   $(LOCAL_PATH)/recovery/bootrec-device-fs:root/sbin/bootrec-device-fs \
+   $(LOCAL_PATH)/recovery.fstab:root/recovery.fstab
+
+# Key layouts and touchscreen
+PRODUCT_COPY_FILES += \
+   $(LOCAL_PATH)/config/AB8500_Hs_Button.kl:system/usr/keylayout/AB8500_Hs_Button.kl \
+   $(LOCAL_PATH)/config/simple_remote.kl:system/usr/keylayout/simple_remote.kl \
+   $(LOCAL_PATH)/config/simple_remote_appkey.kl:system/usr/keylayout/simple_remote_appkey.kl \
+   $(LOCAL_PATH)/config/cyttsp_key.kl:system/usr/keylayout/cyttsp_key.kl \
+   $(LOCAL_PATH)/config/STMPE-keypad.kl:system/usr/keylayout/STMPE-keypad.kl \
+   $(LOCAL_PATH)/config/tc3589x-keypad.kl:system/usr/keylayout/tc3589x-keypad.kl \
+   $(LOCAL_PATH)/config/ux500-ske-keypad.kl:system/usr/keylayout/ux500-ske-keypad.kl.kl \
+   $(LOCAL_PATH)/config/ux500-ske-keypad-qwertz.kl:system/usr/keylayout/ux500-ske-keypad-qwertz.kl \
+   $(LOCAL_PATH)/config/cyttsp-spi.idc:system/usr/idc/cyttsp-spi.idc \
+   $(LOCAL_PATH)/config/sensor00_f11_sensor0.idc:system/usr/idc/sensor00_f11_sensor0.idc \
+   $(LOCAL_PATH)/config/synaptics_rmi4_i2c.idc:system/usr/idc/synaptics_rmi4_i2c.idc
+
+# Misc configuration files
+PRODUCT_COPY_FILES += \
+   $(LOCAL_PATH)/config/manuf_id.cfg:system/etc/AT/manuf_id.cfg \
+   $(LOCAL_PATH)/config/model_id.cfg:system/etc/AT/model_id.cfg \
+   $(LOCAL_PATH)/config/system_id.cfg:system/etc/AT/system_id.cfg \
+   $(LOCAL_PATH)/config/cflashlib.cfg:system/etc/cflashlib.cfg \
+   $(LOCAL_PATH)/config/flashled_param_config.cfg:system/etc/flashled_param_config.cfg
+
+$(call inherit-product, frameworks/base/build/phone-hdpi-512-dalvik-heap.mk)
+
+$(call inherit-product-if-exists, vendor/sony/kumquat/kumquat-vendor.mk)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=240
