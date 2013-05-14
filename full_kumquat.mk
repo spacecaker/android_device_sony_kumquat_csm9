89321e76d6e45ed7124627d7466ebe1842132860
spacecaker <robin-barry@hotmail.com>
2013-05-14 18:35:09 +0300

    Update full_kumquat.mk

--- full_kumquat.mk
+++ full_kumquat.mk
@@ -24,7 +24,8 @@
  
 # Torch
 PRODUCT_PACKAGES := \
-    Torch
+    Torch \
+    PHOENIXWallpapers
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
